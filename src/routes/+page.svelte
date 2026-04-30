ea6f4d061039380f4ea776f5c38bac8c9320fc8a
Tyler Beach <tjb3141@gmail.com>
2026-04-30 09:00:28 -0400

    drop confusing pill

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,11 +60,7 @@
 				<span></span>
 				<span></span>
 			</div>
-			<p
-				class="rounded-full border border-white/10 bg-white/5 px-4 py-2 text-xs font-bold text-cyan-100"
-			>
-				6 digit room code
-			</p>
+
 		</div>
 	</section>
 
